80a0c812602194e2982320e63e310b1317398721
rlksa <rlksa@example.com>
2026-06-28 21:36:43 -0500

    fix: CORS allow all origins

--- core-backend/main.py
+++ core-backend/main.py
@@ -13,12 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://caja-tacna-l4hv.vercel.app",
-        "https://caja-tacna-l4hv-mhya4zdtf-daaam.vercel.app",
-        "https://caja-tacna-l4hv-git-main-daaam.vercel.app",
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
