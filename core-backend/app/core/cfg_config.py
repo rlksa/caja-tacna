533e5846ccff8e3c9c97e5e86ee6730fd26b7806
rlksa <rlksa@example.com>
2026-06-28 19:39:43 -0500

    fix: apuntar api del core a render

--- core-backend/app/core/cfg_config.py
+++ core-backend/app/core/cfg_config.py
@@ -5,7 +5,7 @@ class Settings(BaseSettings):
     SECRET_KEY: str
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
-    PORTAL_BACKEND_URL: str = "http://localhost:8000"
+    PORTAL_BACKEND_URL: str = "https://homebanking-backend-q6fe.onrender.com"
     PORT: int = 8001
 
     class Config:
